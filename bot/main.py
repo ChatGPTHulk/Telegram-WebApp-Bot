b21b6ae1f39b30baea340a3b87d718ec9123cc46
ChatGPTHulk <139841442+ChatGPTHulk@users.noreply.github.com>
2023-10-26 00:53:57 +0530

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,76 +1,38 @@
 import asyncio
 import logging
-from datetime import datetime, timedelta
-
+from aiohttp import web
 import aiohttp_jinja2
 import jinja2
-from aiogram import Dispatcher
+from aiogram import Bot, Dispatcher, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters import CommandStart
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, WebAppInfo
 from aiogram.utils import executor
-from aiogram.utils.exceptions import ChatNotFound
-from aiohttp import web
-from aiohttp.web_request import Request
-from aiohttp.web_response import json_response
+from environs import Env
+BOT_TOKEN = "6237932576:AAFiXs2J4caQG1aGycoaah0IuRU2IsMe5Dc"
 
-from config import ENDPOINT, dp
-from web_app import safe_parse_webapp_init_data
+ENDPOINT = "https://www.canva.com/brand/join?token=2YjFf01xvIS38UY23l6nyA&referrer=team-invite"
+
+# Initialize bot and dispatcher
+bot = Bot(token=BOT_TOKEN)
+storage = MemoryStorage()
+dp = Dispatcher(bot=bot, storage=storage)
 
 logging.basicConfig(level=logging.ERROR)
 
-SEND_MESSAGE_DELTA = {}  # Prevent flood by `Send Message Function`
-
-
-# /web-start
+# Placeholder function for web_start
 async def web_start(request):
-    return await aiohttp_jinja2.render_template_async('start/start.html', request, {})
+    # Your implementation here
+    return web.Response(text="Web Start Placeholder")
 
+# Placeholder function for web_send_message
+async def web_send_message(request):
+    # Your implementation here
+    return web.Response(text="Web Send Message Placeholder")
 
-# /checkUserData
-async def web_check_user_data(request: Request):
-    data = await request.post()
-    data = safe_parse_webapp_init_data(dp.bot._token, data["_auth"])
-    return json_response({"ok": True, "user": data.as_json()})
-
-
-# /sendMessage
-async def web_send_message(request: Request):
-    data = await request.post()
-
-    try:
-        web_app_data = safe_parse_webapp_init_data(token=dp.bot._token, init_data=data["_auth"])
-    except ValueError:
-        return json_response({"ok": False, "error": "Unauthorized"}, status=401)
-
-    web_app_user_id = web_app_data["user"]["id"]
-
-    if web_app_user_id not in SEND_MESSAGE_DELTA:
-        SEND_MESSAGE_DELTA.update({web_app_user_id: datetime.utcnow() - timedelta(seconds=100)})
-
-    delta = SEND_MESSAGE_DELTA.get(web_app_user_id)
-
-    if (datetime.utcnow() - delta) < timedelta(seconds=5):
-        return json_response({"ok": False, "error": "🥶 You are to fast. Please wait for 5 seconds"})
-
-    user_id, text = data.get("user_id"), data.get("text")
-
-    if user_id is None or not text:
-        return json_response({"ok": False, "error": "💁‍♂️ UserID and Text inputs required"})
-
-    try:
-        SEND_MESSAGE_DELTA.update({web_app_user_id: datetime.utcnow()})
-        await dp.bot.send_message(chat_id=user_id, text=text)
-
-    except ChatNotFound:
-        return json_response({"ok": False, "error": "Chat Not Found"})
-
-    except Exception as exc:
-        print(exc)
-        return json_response({"ok": False, "error": "Exception caused"})
-
-    else:
-        return json_response({"ok": True})
-
+# Placeholder function for web_check_user_data
+async def web_check_user_data(request):
+    # Your implementation here
+    return web.Response(text="Web Check User Data Placeholder")
 
 app = web.Application()
 app.add_routes([web.get('/web-start', web_start),
@@ -78,29 +40,62 @@ app.add_routes([web.get('/web-start', web_start),
                 web.post('/checkUserData', web_check_user_data)])
 aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader('web'), enable_async=True)
 
-
 async def on_startup(dps: Dispatcher):
     loop = asyncio.get_event_loop()
-    loop.create_task(web._run_app(app, host="0.0.0.0", port=45678))  # WARN: Don't do this in production!
-
+    loop.create_task(web._run_app(app, host="0.0.0.0", port=45678))
 
 async def on_shutdown(dps: Dispatcher):
     await dps.storage.close()
     await dps.storage.wait_closed()
 
+import datetime
+
+import asyncio
+
+# Define a function to delete buttons after a specified delay
+async def delete_buttons(chat_id, message_id, delay_minutes):
+    await asyncio.sleep(delay_minutes * 60)  # Convert minutes to seconds
+    await bot.delete_message(chat_id, message_id)
 
 @dp.message_handler(CommandStart())
-@dp.throttled(rate=2)
-async def cmd_start(msg: Message):
-    keyboard = InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text="😎 WEB APP", web_app=WebAppInfo(url=f"{ENDPOINT}/web-start"))]
-    ])
-    await msg.reply("TEST WEB APP", reply_markup=keyboard)
-
+async def cmd_start(msg: types.Message):
+    user_id = msg.from_user.id
+    is_member_abhibots = await bot.get_chat_member("@abhibots", user_id)
+    is_member_abyproof = await bot.get_chat_member("@abyproof", user_id)
+    
+    if (is_member_abhibots.status == "member" or is_member_abhibots.status == "administrator" or is_member_abhibots.status == "creator") and \
+       (is_member_abyproof.status == "member" or is_member_abyproof.status == "administrator" or is_member_abyproof.status == "creator"):
+        
+        # Provide instructions for Canva login
+        login_instructions = "To join the Canva team, please log in using your email and enter the OTP you receive."
+        
+        keyboard = types.InlineKeyboardMarkup(inline_keyboard=[
+            [types.InlineKeyboardButton(text="😎 Join Canva Team", web_app=types.WebAppInfo(url=f"{ENDPOINT}"))]
+        ])
+        
+        # Send the instructions message
+        instructions_message = await msg.reply(login_instructions, reply_markup=keyboard)
+        
+        # Schedule the deletion of buttons after 10 minutes
+        asyncio.create_task(delete_buttons(instructions_message.chat.id, instructions_message.message_id, delay_minutes=10))
+        
+    else:
+        # Create inline keyboard buttons to join channels
+        join_abhibots_button = types.InlineKeyboardButton("Join @abhibots", url="https://t.me/abhibots")
+        join_abyproof_button = types.InlineKeyboardButton("Join @abyproof", url="https://t.me/abyproof")
+        
+        keyboard = types.InlineKeyboardMarkup(inline_keyboard=[
+            [join_abhibots_button, join_abyproof_button]
+        ])
+        
+        # Send the instructions message
+        instructions_message = await msg.reply("To use this bot, please join both @abhibots and @abyproof channels by clicking the respective links below and then restart the bot.", reply_markup=keyboard)
+        
+        # Schedule the deletion of buttons after 10 minutes
+        asyncio.create_task(delete_buttons(instructions_message.chat.id, instructions_message.message_id, delay_minutes=10))
 
 def main():
     executor.start_polling(dp, on_startup=on_startup, on_shutdown=on_shutdown)
-
 
 if __name__ == "__main__":
     main()
