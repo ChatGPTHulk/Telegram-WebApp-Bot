9693d43eb1ee224a4294c32535b14493f8032ad1
ChatGPTHulk <139841442+ChatGPTHulk@users.noreply.github.com>
2023-10-26 17:34:45 +0530

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -29,7 +29,7 @@ Please use these commands responsibly!
     
     # Create an inline keyboard with a "Add to Group" button
     keyboard = InlineKeyboardMarkup()
-    add_to_group_button = InlineKeyboardButton("Add to Group", url=f"https://t.me/banbyvotebot?startgroup=start")
+    add_to_group_button = InlineKeyboardButton("Add to Group", url=f"https://t.me/banbyvotebot?start=invite")
     keyboard.add(add_to_group_button)
 
     await message.answer(welcome_text, reply_markup=keyboard)
